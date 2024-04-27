d1a2e522b6b1ccf84caf472bbdd800f35194c8a0
ahmadile <76616331+ahmadile@users.noreply.github.com>
2024-04-27 22:21:04 +0100

    new style

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
   theme: {
     extend: {
       lineHeight:{
-        'header-heigth':'3rem',
+        // 'header-heigth':'3rem',
       },
       colors:{
         'dark-color':'#141414',
@@ -20,6 +20,7 @@ export default {
       },
       fontSize:{
         'big':'1.25rem',
+        'fz':'1.3rem',
         'bigger':'1.5rem',
         'biggest':'2rem',
         'h2':'1.25rem',
@@ -28,6 +29,15 @@ export default {
       },
       fontFamily:{
         'body':['Poppins'],
+      },
+      zIndex: {
+        'z-fixed': '100',
+      },
+      gridTemplateColumns:{
+        'template-c':'100%'
+      },
+      rotate: {
+        'rotate-img': '35deg',
       }
     },
   },
